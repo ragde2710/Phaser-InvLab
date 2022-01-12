455970ab98d638cb7b2ef031c4616af9a83b80b3
Edgar Ivan Garcia Mancillas <edgar.mancillas@alumnos.udg.mx>
2022-01-12 12:12:31 -0600

    Keyboard control

--- src/init.js
+++ src/init.js
@@ -12,7 +12,7 @@ const config = {
         default: "arcade",
         arcade: {
             gravity: {
-                y: 500
+                //y: 500
             }
         }
     }
@@ -24,15 +24,28 @@ function preload() {
     this.load.image("pajaro", "./assets/bird.png")
 }
 function create() {
-    this.pajaro = this.physics.add.image(50, 100, "pajaro");
-    //Manipulacion de la imagen
+    this.pajaro = this.physics.add.image(100, 50, "pajaro");
+    this.right = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.left =  this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+    /*
+    Manipulacion de la imagen
+
     this.pajaro.setScale(1.5);
     this.pajaro.flipX = false;
     this.pajaro.setOrigin(0.5);
-    //Fisicas
+
+    Fisicas
+
     this.pajaro.setCollideWorldBounds(true);
     this.pajaro.setBounce(0.5);
     this.pajaro.setVelocity(50, 0);
+    */
+;
 }
-function update() {
+function update(time, delta) {
+    if(this.right.isDown) {
+        this.pajaro.x++;
+    } else if (this.left.isDown){
+        this.pajaro.x--;
+    }
 }
